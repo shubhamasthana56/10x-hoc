d824ff0a66ad356cf8e0fd86b8b7e1818e0a2818
shubham asthana <shubhamasthana56@gmail.com>
2022-06-23 11:40:10 +0530

    hoc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './sidebar';
+import Header from './header';
+import HOC from './hoc';
+import About from './about';
+import Layout from './hoc';
+import Contact from './contact';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
+  const ContactComponent = Layout(Contact);
+  const AboutComponent = Layout(About);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <BrowserRouter>
+        <Routes>
+          <Route path='/about' element={<AboutComponent/>}></Route>
+          <Route path='/contact' element={<ContactComponent/>}></Route>
+          <Route path='/login' element={<div>Login Works</div>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
+     
+ 
   );
 }
 
